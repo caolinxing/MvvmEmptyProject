ccf192c5a07fd86f7813f2a410f276e83ef286bf
caolinxing <1533406071@qq.com>
2021-07-04 19:25:25 +0800

    修改底部icon part_7.1

--- module-main/src/main/java/com/zhulong/main/ui/SplashActivity.java
+++ module-main/src/main/java/com/zhulong/main/ui/SplashActivity.java
@@ -24,6 +24,7 @@ public class SplashActivity extends AppCompatActivity {
                     MainActivity.start(this);
                 }*/
                 MainActivity.start(SplashActivity.this);
+                finish();
 
             }
         },1000);
